1e3ed1f9c4d52627e8d664a6ab6763d9f160e77d
Ronaldinho-OrtegaRuiz <rortegar01@libertadores.edu.co>
2024-03-15 21:59:08 -0500

    Primer commit

---
+++ src/main/java/com/ronaldrogas/ronaldrogasbackend/RonaldrogasBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ronaldrogas.ronaldrogasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RonaldrogasBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RonaldrogasBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ronaldrogas/ronaldrogasbackend/RonaldrogasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronaldrogas.ronaldrogasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RonaldrogasBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
